27f9567c1343b955b5a418f37da62c0dcf6f1569
Instituto Milenio BASE <130472788+instituto-base@users.noreply.github.com>
2026-02-17 17:33:19 -0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,20 +17,32 @@ var crs = new L.Proj.CRS(
 );
 
 var map = L.map('map', {
-	crs: crs,
-}).setView([-90, 166.666667], 1.5);
+    crs: crs,
+    minZoom: 1,      // 👈 evita zoom demasiado lejano
+    maxZoom: 8,      // ajusta si necesitas más detalle
+    maxBoundsViscosity: 1.0
+}).setView([-90, 166.666667], 2);  // 👈 usa entero, 1.5 puede causar requests raros
 
-L.tileLayer('https://tile.gbif.org/3031/omt/{z}/{x}/{y}@{r}x.png?style=gbif-classic'.replace('{r}', pixel_ratio), {
-	tileSize: 512
-}).addTo(map);
 
-
-  // 4. Carga de datos (Igual que antes)
+// 🔹 Fondo base GBIF
 L.tileLayer(
-'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@2x.png?srs=EPSG:3031&bin=hex&hexPerTile=97&publishingOrg=29ef4f00-20db-41f8-b1ad-b5fd3c557c38&style=iNaturalist.poly'
+  'https://tile.gbif.org/3031/omt/{z}/{x}/{y}@{r}x.png?style=gbif-classic'
     .replace('{r}', pixel_ratio),
   {
     tileSize: 512,
-    opacity: 0.8
+    noWrap: true,          // 👈 MUY IMPORTANTE en proyecciones polares
+    errorTileUrl: ''       // 👈 evita mostrar imagen de error
+  }
+).addTo(map);
+
+
+// 🔹 Capa de densidad
+L.tileLayer(
+  'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@2x.png?srs=EPSG:3031&bin=hex&hexPerTile=97&publishingOrg=29ef4f00-20db-41f8-b1ad-b5fd3c557c38&style=iNaturalist.poly',
+  {
+    tileSize: 512,
+    opacity: 0.8,
+    noWrap: true,
+    errorTileUrl: ''
   }
 ).addTo(map);
